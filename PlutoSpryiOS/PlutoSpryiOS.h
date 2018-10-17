7a310c5208476ee694a3e0fd8761328bf15756a6
MEGHA GULATI <meghagulati@MEGHAs-MBP.genui.co>
2018-10-17 14:29:21 -0700

    Adding build framework

---
+++ PlutoSpryiOS/PlutoSpryiOS.h
@@ -0,0 +1,19 @@
+//
+//  PlutoSpryiOS.h
+//  PlutoSpryiOS
+//
+//  Created by MEGHA GULATI on 10/17/18.
+//  Copyright © 2018 Pluto VR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlutoSpryiOS.
+FOUNDATION_EXPORT double PlutoSpryiOSVersionNumber;
+
+//! Project version string for PlutoSpryiOS.
+FOUNDATION_EXPORT const unsigned char PlutoSpryiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlutoSpryiOS/PublicHeader.h>
+
+
